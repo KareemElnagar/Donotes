a3322ad36aa1e9e63b94b3afa3cef51d9b55822e
Kareem Elnagar <kareemelnagar733@gmail.com>
2024-02-07 23:11:04 +0200

    dependencies and binding

--- app/src/main/java/com/kareem/donotes/MainActivity.kt
+++ app/src/main/java/com/kareem/donotes/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kareem.donotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kareem.donotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
