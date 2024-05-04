81d5b5a716ef3dc6f678b7c3e80aab586a8ef553
Kareem Elnagar <kareemelnagar733@gmail.com>
2024-05-04 14:05:49 +0200

    search note

--- app/src/main/java/com/kareem/donotes/HomeFragment.kt
+++ app/src/main/java/com/kareem/donotes/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.kareem.donotes
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.kareem.donotes.adapter.NotesAdapter
@@ -9,6 +10,7 @@ import com.kareem.donotes.database.NotesDatabase
 import com.kareem.donotes.databinding.FragmentHomeBinding
 import com.kareem.donotes.entities.Notes
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
@@ -19,14 +21,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         FragmentHomeBinding::inflate
 
 
-
-
     private val onClicked = object : NotesAdapter.OnItemClickListener {
         override fun onClick(notesId: Int) {
-            var fragment : Fragment
+            var fragment: Fragment
             var bundle = Bundle()
-            bundle.putString("edit","isEdit")
-            bundle.putInt("notesId",notesId)
+            bundle.putString("edit", "isEdit")
+            bundle.putInt("notesId", notesId)
             fragment = CreateNoteFragment()
             fragment.arguments = bundle
             replaceFragment(fragment, true)
@@ -51,11 +51,31 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
                     ).getAllNotes()
 
                     notesAdapter!!.setData(notes)
+                    arrNotes = notes as ArrayList<Notes>
                     recyclerView.adapter = notesAdapter
                 }
 
 
             }
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+                androidx.appcompat.widget.SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(p0: String?): Boolean {
+                    var tempArr = ArrayList<Notes>()
+                    for (arr in arrNotes) {
+                        if (arr.title!!.lowercase(Locale.getDefault()).contains(p0.toString())) {
+                            tempArr.add(arr)
+                        }
+                    }
+                    notesAdapter.setData(tempArr)
+                    notesAdapter.notifyDataSetChanged()
+                    return true
+                }
+
+            })
 
             notesAdapter!!.setOnClickListener(onClicked)
         }
